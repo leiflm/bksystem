323da53c831c4f2b428ef7c16535341ece7fb2f9
Leif Middelschulte <Leif.Middelschulte@gmail.com>
2012-10-25 06:40:46 +0000

    Add documentation for Bks_Job

--- src/include/Bks_Job.h
+++ src/include/Bks_Job.h
@@ -4,6 +4,20 @@
 #include "Bks_Status.h"
 #include "Bks_Types.h"
 
+/*
+ * Bks_Job is an entity used to prevent thread race conditions.
+ * Imagine the following schedule of threaded events:
+ *
+ * Start sequence:
+ * 1.) Clear user list
+ * 2.) Get new user list
+ * Actually executed sequence:
+ * 1.) Got new user list
+ * 2.) Cleared new user list
+ *
+ * It furthermore allows us to easily reschedule jobs if e.g. the database file
+ * is not available.
+ */
 enum _Bks_Job_Type { BKS_JOB_SALE,
                      BKS_JOB_PRODUCTS_ALPHA_GET,
                      BKS_JOB_PRODUCTS_FAVS_GET,
