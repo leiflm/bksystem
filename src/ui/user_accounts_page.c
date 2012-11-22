f9dc9b2707741280693f0a5d2cbb4446df07fba2
Leif Middelschulte <Leif.Middelschulte@gmail.com>
2012-11-23 00:23:37 +0100

    Clear user list before reloading filtered list.

--- src/ui/user_accounts_page.c
+++ src/ui/user_accounts_page.c
@@ -13,6 +13,7 @@ _on_user_accounts_sort_click(void *data UNUSED, Evas_Object *obj UNUSED, void *e
 {
     Bks_Ui_Filter_Mode fm = (Bks_Ui_Filter_Mode)data;
 
+    bks_ui_controller_user_accounts_clear();
     printf("Sort Cb!\n");
     switch (fm)
     {
