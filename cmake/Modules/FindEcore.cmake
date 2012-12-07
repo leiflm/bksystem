49d7dccab39a0eae77584a2891f3c28e2fc7c4a5
Leif Middelschulte <Leif.Middelschulte@gmail.com>
2012-12-07 13:41:26 +0100

    Split Ecore_File into own cmake module, so we can set requirement.

--- cmake/Modules/FindEcore.cmake
+++ cmake/Modules/FindEcore.cmake
@@ -61,24 +61,3 @@ if (ECORE_USE_ECORE-X)
 
    mark_as_advanced( ECORE_X_INCLUDE_DIR ECORE_X_LIBRARY )
 endif (ECORE_USE_ECORE-X)
-
-pkg_check_modules(PC_LIBECORE_FILE ${_QUIET} ecore-file)
-set(ECORE_FILE_DEFINITIONS ${PC_LIBECORE_FILE_CFLAGS_OTHER})
-
-find_path(ECORE_FILE_INCLUDE_DIR Ecore_File.h
-          HINTS ${PC_LIBECORE_FILE_INCLUDEDIR} ${PC_LIBECORE_FILE_INCLUDE_DIRS}
-          PATH_SUFFIFILEES ecore )
-
-find_library(ECORE_FILE_LIBRARY NAMES ecore_file
-             HINTS ${PC_LIBECORE_FILE_LIBDIR} ${PC_LIBECORE_FILE_LIBRARY_DIRS} )
-
-set(ECORE_FILE_LIBRARIES ${ECORE_FILE_LIBRARY} )
-set(ECORE_FILE_INCLUDE_DIRS ${ECORE_FILE_INCLUDE_DIR} )
-
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set ECORE_FILE_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(ecore_file  DEFAULT_MSG
-                                  ECORE_FILE_LIBRARY ECORE_FILE_INCLUDE_DIR)
-
-mark_as_advanced( ECORE_FILE_INCLUDE_DIR ECORE_FILE_LIBRARY )
